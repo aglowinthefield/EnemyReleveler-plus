c5ac18e73972a1d5a43d8b102c49060287f83a65
aglowinthefield <tom.auty+nexus@gmail.com>
2024-01-07 17:48:31 -0500

    Refactor and rename. Add settings support

---
+++ EnemyRelevelerPlus/EnemyRelevelerPlusSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace EnemyRelevelerPlus;
+public class EnemyRelevelerPlusSettings
+{
+    [SettingName("Mods to exclude")]
+    [Tooltip("These mods raise NPC levels on their own")]
+    public HashSet<ModKey> ModsToExclude = [];
+}
